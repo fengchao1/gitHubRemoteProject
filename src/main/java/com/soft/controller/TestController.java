92278ca034b11f6b9312ad279144e871c2477fe2
fengchao1 <376212842@qq.com>
2019-06-06 17:41:11 +0800

    egit05

--- src/main/java/com/soft/controller/TestController.java
+++ src/main/java/com/soft/controller/TestController.java
@@ -3,6 +3,7 @@ package com.soft.controller;
 public class TestController {
 	public static void main(String[] args) {
 		System.out.println("789111111");
+		System.err.println("4561");
 		System.err.println("456");
 	}
 }
