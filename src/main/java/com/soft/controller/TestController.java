90c134b1c3894bc01e1b95d67cabaaff10c2ee2b
fengchao1 <376212842@qq.com>
2019-06-10 11:15:29 +0800

    重新测试

--- src/main/java/com/soft/controller/TestController.java
+++ src/main/java/com/soft/controller/TestController.java
@@ -2,6 +2,7 @@ package com.soft.controller;
 
 public class TestController {
 	public static void main(String[] args) {
+		System.err.println("456");
 	}
 	public void helloworld(){
 	}
--- src/main/java/com/soft/controller/TestController1.java
+++ src/main/java/com/soft/controller/TestController1.java
@@ -2,6 +2,7 @@ package com.soft.controller;
 
 public class TestController1 {
 	public static void main(String[] args) {
+		System.err.println("123");
 	}
 	public void helloworld(){
 	}
