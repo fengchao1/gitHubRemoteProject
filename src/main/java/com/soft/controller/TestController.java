d59333020ca0118592816f7338126093047825d1
fengchao1 <376212842@qq.com>
2019-06-10 11:26:44 +0800

    重置

--- src/main/java/com/soft/controller/TestController.java
+++ src/main/java/com/soft/controller/TestController.java
@@ -2,13 +2,7 @@ package com.soft.controller;
 
 public class TestController {
 	public static void main(String[] args) {
-		System.err.println("123");
 	}
 	public void helloworld(){
-<<<<<<< HEAD
-		System.err.println("123");
-=======
-		System.err.println("456");
->>>>>>> branch 'master' of https://github.com/fengchao1/gitHubRemoteProject.git
 	}
 }
