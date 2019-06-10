b1b47b6a7f03da6b5ad40cc0a8942b052b82a0e5
fengchao1 <376212842@qq.com>
2019-06-10 10:56:26 +0800

    代码重置

--- src/main/java/com/soft/controller/TestController.java
+++ src/main/java/com/soft/controller/TestController.java
@@ -2,20 +2,7 @@ package com.soft.controller;
 
 public class TestController {
 	public static void main(String[] args) {
-		System.out.println("789111111");
-		System.err.println("4561");
-		System.err.println("456");
-		System.err.println("123");
-		System.err.println("456");
-		System.err.println("987");//456
 	}
 	public void helloworld(){
-		System.err.println("i'm hellokitty");
-		System.err.println("who are you?");
-		System.err.println("sorry,I'm don't talk you!");
-		System.err.println("really?");
-		System.err.println("really?" +"1");
-		System.err.println("really?" +"2");
-		System.err.println("really?" +"3");
 	}
 }
--- src/main/java/com/soft/controller/TestController1.java
+++ src/main/java/com/soft/controller/TestController1.java
@@ -2,15 +2,7 @@ package com.soft.controller;
 
 public class TestController1 {
 	public static void main(String[] args) {
-		System.out.println("789111111");
-		System.err.println("4561");
-		System.err.println("456");
-		System.err.println("456");
 	}
 	public void helloworld(){
-        System.err.println("i'm hellokitty"+"0");
-		System.err.println("i'm hellokitty"+"1");
-		System.err.println("i'm hellokitty"+"2");
-		System.err.println("i'm hellokitty"+"3");
 	}
 }
